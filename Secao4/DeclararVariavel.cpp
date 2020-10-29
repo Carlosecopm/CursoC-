7989696898dbe782000e70c6d320e9db426f7c00
Carlosecopm <carlosluizecomp.uefs@gmail.com>
2020-10-29 17:30:16 -0300

    Finalizada a seçao 4

---
+++ Secao4/DeclararVariavel.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+int main()
+{
+    int NumVidas = 5;
+    int Score = 1350;
+    std::cout << "*************" << std::endl;
+    std::cout << "Vidas Jogador: " << NumVidas << std::endl;
+    std::cout << "Score: " << Score << std::endl;
+    return 0;
+}
